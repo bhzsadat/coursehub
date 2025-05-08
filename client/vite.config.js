861810bfcd886d06f0fc84fd202342fb8502288f
Behnaz Sadat <behnaz@Behnazs-MacBook-Pro.local>
2025-05-07 23:05:38 -0700

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,4 +12,15 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    minify: 'terser',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
